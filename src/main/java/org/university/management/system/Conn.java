224a89222a1290674768ce5783ec80a7ba3eadcf
Hasanov23 <zamilhh@code.edu.az>
2023-05-14 16:24:54 +0200

    Initial commit

---
+++ src/main/java/org/university/management/system/Conn.java
@@ -0,0 +1,20 @@
+package org.university.management.system;
+
+import java.sql.*;
+
+public class Conn {
+
+    Connection c;
+    Statement s;
+
+    Conn () {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///universitymanagementsystem", "root", "swehasanov");
+            s = c.createStatement();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
